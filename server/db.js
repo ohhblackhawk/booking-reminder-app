0602c9c3ccfae8cec616f803a26253608d8da7e6
ohhblackhawk <jamesthapa@hotmail.com>
2025-03-26 01:46:39 +0000

    Add PostgreSQL connection

---
+++ server/db.js
@@ -0,0 +1,13 @@
+require('dotenv').config({ path: '../.env'});
+const Pool = require("pg").Pool;
+
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+});
+
+module.exports = pool;
