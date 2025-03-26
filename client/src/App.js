bc719d9215fd2b1039c01cd06baa277817e2781d
ohhblackhawk <jamesthapa@hotmail.com>
2025-03-26 02:11:03 +0000

    Setting Up React Client

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { Fragment } from "react";
+import logo from './logo.svg';
+import './App.css';
+
+// components
+import InputTodo from "./components/InputTodo";
+import ListTodos from "./components/ListTodo";
+
+function App() {
+
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo />
+        <ListTodos />
+      </div>
+
+    </Fragment>
+  );
+}
+
+export default App;
